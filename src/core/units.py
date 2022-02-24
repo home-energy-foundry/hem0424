760a50e9f230bd2e6c3c2b64d039e9fbabd22354
Steven Abnett <steven.abnett@bregroup.com>
2022-02-24 16:38:40 +0000

    Add module for unit conversions

---
+++ src/core/units.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+""" TODO Copyright & licensing notices
+
+This module contains common unit conversions for use by other modules.
+"""
+
+J_per_kWh = 3600000
+minutes_per_hour = 60
