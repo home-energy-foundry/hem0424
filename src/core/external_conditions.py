eda565acda3c1b2b2307921ed4e979ae4486497e
Steven Abnett <steven.abnett@bregroup.com>
2022-03-08 20:45:53 +0000

    Add module to store and look up data on external conditions
    Currently only handles external air temperature data. Extend in future
    to handle other aspects of external conditions (e.g. solar radiation).

---
+++ src/core/external_conditions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+""" TODO Copyright & licensing notices
+
+This module provides object(s) to store and look up data on external conditions
+(e.g. external air temperature)
+"""
+
+class ExternalConditions:
+    """ An object to store and look up data on external conditions """
+
+    def __init__(self, simulation_time, air_temps):
+        """ Construct an ExternalConditions object
+
+        Arguments:
+        simulation_time -- reference to SimulationTime object
+        air_temps       -- list of external air temperatures, in deg C (one entry per timestep)
+        """
+        self.__simulation_time  = simulation_time
+        self.__air_temps        = air_temps
+
+    def air_temp(self):
+        """ Return the external air temperature for the current timestep """
+        return self.__air_temps[self.__simulation_time.index()]
+        # TODO Assumes air temps list is one entry per timestep but in
+        #      future it could be e.g. hourly figures even if timestep is
+        #      sub-hourly. This would require the SimulationTime class to
+        #      support such lookups.
