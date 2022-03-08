6ae33423b32041d3f8b5770b88d9bcd9ac230316
Steven Abnett <steven.abnett@bregroup.com>
2022-03-08 19:51:41 +0000

    Revise comments in building_element module

--- src/core/space_heat_demand/building_element.py
+++ src/core/space_heat_demand/building_element.py
@@ -3,8 +3,8 @@
 """ TODO Copyright & licensing notices
 
 This module provides objects to represent building elements such as walls,
-floors and windows. Each of these building elements is made up of 2 or more
-nodes (at boundaries between layers) and is associated with a thermal zone.
+floors and windows. Each of these building elements has 2 or more nodes and is
+associated with a thermal zone.
 
 Note that the temperatures at each node for each timestep of the calculation
 are calculated and stored in the zone module, not here. This is based on the
@@ -17,6 +17,16 @@ class BuildingElement:
     Classes for particular types of building element should inherit from this
     one and add/override functionality as required. It is not intended for
     objects of this class to be used directly.
+    
+    Subclasses should calculate/implement (at least) the following:
+    self.h_pli      -- list (len = number of nodes - 1) of thermal conductances,
+                       in W / (m2.K) . Element 0 will be conductance between nodes
+                       0 and 1. Calculate according to BS EN ISO 52016-1:2017,
+                       section 6.5.7
+    self.k_pli      -- list of areal heat capacities for each node, in J / (m2.K)
+                       Calculate according to BS EN ISO 52016-1:2017, section 6.5.7
+    self.temp_ext() -- function to return the temperature of the external
+                       environment, in deg C
     """
 
     def __init__(self, area, h_ci, h_ri, h_ce, h_re, a_sol):
@@ -30,7 +40,6 @@ class BuildingElement:
         h_re  -- external radiative heat transfer coefficient, in W / (m2.K)
         a_sol -- solar absorption coefficient at the external surface (dimensionless)
         """
-
         self.area  = area
         self.h_ci  = h_ci
         self.h_ri  = h_ri
@@ -40,17 +49,6 @@ class BuildingElement:
 
         # TODO beta_eli and gamma_eli are defined under eqn 41 in BS EN ISO 52016-1:2017.
         #      Where are they used?
-
-        # List of thermal conductances (len = number of nodes - 1). Element 0
-        # will be conductance between nodes 0 and 1.
-        # Init these figures in relevant subclass __init__ func according to
-        # BS EN ISO 52016-1:2017, section 6.5.7, not here.
-        # self.h_pli =
-
-        # List of areal heat capacities for each node
-        # Init these figures in relevant subclass __init__ func according to
-        # BS EN ISO 52016-1:2017, section 6.5.7, not here.
-        # self.k_pli =
 
     def no_of_nodes(self):
         """ Return number of nodes including external and internal layers """
