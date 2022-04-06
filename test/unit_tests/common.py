9875d4aca1a6d724193cc8c5e468567cef427d6a
Steven Abnett <steven.abnett@bregroup.com>
2022-04-06 18:21:33 +0100

    Add code to configure environment for unit tests

---
+++ test/unit_tests/common.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+""" TODO Copyright & licensing notices
+
+This module contains functionality that is shared between unit tests
+"""
+
+import sys
+import os
+
+def test_setup():
+    """Set up test environment"""
+
+    # Add src folder to path so that packages can be imported into tests
+    this_dir = os.path.dirname(__file__)
+    proj_path = os.path.dirname(os.path.dirname(this_dir))
+    src_path = os.path.join(proj_path, 'src')
+    sys.path.append(os.path.abspath(src_path))
