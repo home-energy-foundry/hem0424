6ef9deb6a12998b029e68fc6330da60bcff055bd
Steven Abnett <steven.abnett@bregroup.com>
2022-09-23 15:23:41 +0100

    Separate top-level code to be run per input file into own function
    (in preparation for running multiple files in parallel)

--- src/sap.py
+++ src/sap.py
@@ -17,64 +17,75 @@ from read_weather_file import weather_data_to_dict
 
 # TODO Rewrite this module with argparse library
 
-inp_filename = sys.argv[1]
-file_path = os.path.splitext(inp_filename)
-output_file = file_path[0] + '_results.csv'
+def run_project(inp_filename, external_conditions_dict):
+    file_path = os.path.splitext(inp_filename)
+    output_file = file_path[0] + '_results.csv'
 
-with open(inp_filename) as json_file:
-    project_dict = json.load(json_file)
+    with open(inp_filename) as json_file:
+        project_dict = json.load(json_file)
 
-if len(sys.argv) > 2:
-    project_dict["ExternalConditions"] = weather_data_to_dict(sys.argv[2])
+    if external_conditions_dict is not None:
+        project_dict["ExternalConditions"] = external_conditions_dict
 
-project = Project(project_dict)
-timestep_array, results_totals, results_end_user, zone_dict, zone_list, hc_system_dict = project.run()
+    project = Project(project_dict)
+    timestep_array, results_totals, results_end_user, zone_dict, zone_list, hc_system_dict = project.run()
 
-with open(output_file, 'w') as f:
-    writer = csv.writer(f)
+    with open(output_file, 'w') as f:
+        writer = csv.writer(f)
 
-    headings = ['Timestep']
-    for totals_key in results_totals.keys():
-        totals_header = str(totals_key)
-        totals_header = totals_header + ' total'
-        headings.append(totals_header)
-        for end_user_key in results_end_user[totals_key].keys():
-            headings.append(end_user_key)
+        headings = ['Timestep']
+        for totals_key in results_totals.keys():
+            totals_header = str(totals_key)
+            totals_header = totals_header + ' total'
+            headings.append(totals_header)
+            for end_user_key in results_end_user[totals_key].keys():
+                headings.append(end_user_key)
 
-    for zone in zone_list:
-        for zone_outputs in zone_dict.keys():
-            zone_headings = zone_outputs + ' ' + zone
-            headings.append(zone_headings)
-
-    for system in hc_system_dict:
-        for hc_name in hc_system_dict[system].keys():
-            if hc_name == None:
-                hc_name = 'None'
-                hc_system_headings = system + ' ' + hc_name
-            else:
-                hc_system_headings = system + ' ' + hc_name
-            headings.append(hc_system_headings)
-
-    writer.writerow(headings)
-
-    for t_idx, timestep in enumerate(timestep_array):
-        energy_use_row = []
-        zone_row = []
-        hc_system_row = []
-        i = 0
-        # Loop over end use totals
-        for totals_key in results_totals:
-            energy_use_row.append(results_totals[totals_key][t_idx])
-            for end_user_key in results_end_user[totals_key]:
-                energy_use_row.append(results_end_user[totals_key][end_user_key][t_idx])
-            # Loop over results separated by zone
         for zone in zone_list:
-            for zone_outputs in zone_dict:
-                zone_row.append(zone_dict[zone_outputs][zone][t_idx])
-            # Loop over heating and cooling system demand
-        for system in hc_system_dict:
-            for hc_name in hc_system_dict[system]:
-                hc_system_row.append(hc_system_dict[system][hc_name][t_idx])
+            for zone_outputs in zone_dict.keys():
+                zone_headings = zone_outputs + ' ' + zone
+                headings.append(zone_headings)
 
-        row = [t_idx] + energy_use_row + zone_row + hc_system_row
-        writer.writerow(row)
+        for system in hc_system_dict:
+            for hc_name in hc_system_dict[system].keys():
+                if hc_name == None:
+                    hc_name = 'None'
+                    hc_system_headings = system + ' ' + hc_name
+                else:
+                    hc_system_headings = system + ' ' + hc_name
+                headings.append(hc_system_headings)
+
+        writer.writerow(headings)
+
+        for t_idx, timestep in enumerate(timestep_array):
+            energy_use_row = []
+            zone_row = []
+            hc_system_row = []
+            i = 0
+            # Loop over end use totals
+            for totals_key in results_totals:
+                energy_use_row.append(results_totals[totals_key][t_idx])
+                for end_user_key in results_end_user[totals_key]:
+                    energy_use_row.append(results_end_user[totals_key][end_user_key][t_idx])
+                # Loop over results separated by zone
+            for zone in zone_list:
+                for zone_outputs in zone_dict:
+                    zone_row.append(zone_dict[zone_outputs][zone][t_idx])
+                # Loop over heating and cooling system demand
+            for system in hc_system_dict:
+                for hc_name in hc_system_dict[system]:
+                    hc_system_row.append(hc_system_dict[system][hc_name][t_idx])
+
+            row = [t_idx] + energy_use_row + zone_row + hc_system_row
+            writer.writerow(row)
+
+if __name__ == '__main__':
+    inp_filename = sys.argv[1]
+
+    if len(sys.argv) > 2:
+        external_conditions_dict = weather_data_to_dict(sys.argv[2])
+    else:
+        external_conditions_dict = None
+
+    run_project(inp_filename, external_conditions_dict)
+
