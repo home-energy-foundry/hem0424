61baf8376b3ba56324dfc54bb6695d019748da02
Steven Abnett <steven.abnett@bregroup.com>
2022-10-26 11:32:40 +0100

    Make heating and cooling setpoints per-zone inputs

--- src/core/project.py
+++ src/core/project.py
@@ -452,7 +452,9 @@ class Project:
                 data['volume'],
                 building_elements,
                 thermal_bridging,
-                vent_elements
+                vent_elements,
+                data['temp_setpnt_heat'],
+                data['temp_setpnt_cool'],
                 )
 
         self.__zones = {}
