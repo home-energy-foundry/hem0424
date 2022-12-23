d793d3fec3e82de40ce8c5ec6e8e719eb5566dcb
JoseOrtiz <JoseOrtiz@DESKTOP-V80QQGT.lan>
2022-12-23 20:39:14 +0000

    Init function completed with first set of required inputs

--- src/core/heating_systems/solar_thermal.py
+++ src/core/heating_systems/solar_thermal.py
@@ -9,12 +9,19 @@ Method 3 in BS EN 15316-4-3:2017.
 # Standard library imports
 
 # Local imports
+from core.material_properties import WATER
 import core.units as units
 # TODO MJFC: Any other imports required?
 
 
 class SolarThermalSystem:
     """ An object to represent a solar thermal system """
+
+    #BS EN 15316-4-3:2017 Appendix B default input data
+    #Model Information
+    #Air temperature in a heated space in the building
+    #Default taken from Table B20 of standard
+    __air_temp_heated_room = 20
 
     """ Initially hard code inputs and add them to the input file later """
     #Table 18
@@ -24,12 +31,28 @@ class SolarThermalSystem:
     __SOL_LOC = "NHS"
     
 
-    def __init__(self, peak_power, ventilation_strategy, pitch, orientation,
-                 ext_cond, energy_supply_conn, simulation_time):
+    def __init__(self, 
+                 sol_loc,
+                 area_module,
+                 modules,
+                 peak_collector_efficiency,
+                 incidence_angle_modifier,
+                 first_order_hlc,
+                 second_order_hlc,
+                 collector_mass_flow_rate,
+                 power_pump,
+                 power_pump_control,
+                 tilt,
+                 orientation,
+                 solar_loop_piping_hlc,
+                 ext_cond, 
+                 simulation_time,
+                 contents=WATER,
+                 ):
         """ Construct a SolarThermalSystem object
 
         Arguments:
-        peak_power       -- Peak power in kW; represents the electrical power of a photovoltaic
+        sol_loc          -- Peak power in kW; represents the electrical power of a photovoltaic
                             system with a given area and a for a solar irradiance of 1 kW/m2
                             on this surface (at 25 degrees)
                             TODO - Could add other options at a later stage.
@@ -53,72 +76,31 @@ class SolarThermalSystem:
         simulation_time  -- reference to SimulationTime object
         overshading      -- TODO could add at a later date. Feed into solar module
         """
-        self.__peak_power = peak_power
-        self.__f_perf = self.__f_perf_lookup[ventilation_strategy]
-        self.__pitch = pitch
+        self.__sol_loc = sol_loc
+        self.__area_module = area_module
+        self.__modules = modules
+        self.__area = area_module * modules
+        self.__peak_collector_efficiency = peak_collector_efficiency
+        self.__incidence_angle_modifier = incidence_angle_modifier
+        self.__first_order_hlc = first_order_hlc
+        self.__second_order_hlc = second_order_hlc
+        self.__collector_mass_flow_rate = collector_mass_flow_rate
+        self.__power_pump = power_pump
+        self.__power_pump_control = power_pump_control
+        self.__tilt = tilt
         self.__orientation = orientation
+        self.__solar_loop_piping_hlc = solar_loop_piping_hlc
         self.__external_conditions = ext_cond
-        self.__energy_supply_conn = energy_supply_conn
         self.__simulation_time = simulation_time
-
-#    def __init__(self, peak_power, ventilation_strategy, pitch, orientation,
-#                 ext_cond, energy_supply_conn, simulation_time):
-#        """ Construct a SolarThermalSystem object
-#
-#        Arguments:
-#        peak_power       -- Peak power in kW; represents the electrical power of a photovoltaic
-#                            system with a given area and a for a solar irradiance of 1 kW/m2
-#                            on this surface (at 25 degrees)
-#                            TODO - Could add other options at a later stage.
-#                            Standard has alternative method when peak power is not available
-#                            (input type of PV module and Area instead when peak power unknown)
-#        ventilation_strategy   -- ventilation strategy of the PV system.
-#                                  This will be used to determine the system performance factor
-#                                   based on a lookup table
-#        pitch            -- is the tilt angle (inclination) of the PV panel from horizontal,
-#                            measured upwards facing, 0 to 90, in degrees.
-#                            0=horizontal surface, 90=vertical surface.
-#                            Needed to calculate solar irradiation at the panel surface.
-#        orientation      -- is the orientation angle of the inclined surface, expressed as the
-#                            geographical azimuth angle of the horizontal projection of the inclined
-#                            surface normal, -180 to 180, in degrees;
-#                            Assumed N 180 or -180, E 90, S 0, W -90
-#                            TODO - PV standard refers to angle as between 0 to 360?
-#                            Needed to calculate solar irradiation at the panel surface.
-#        ext_cond         -- reference to ExternalConditions object
-#        energy_supply_conn    -- reference to EnergySupplyConnection object
-#        simulation_time  -- reference to SimulationTime object
-#        overshading      -- TODO could add at a later date. Feed into solar module
-#        """
-#        self.__peak_power = peak_power
-#        self.__f_perf = self.__f_perf_lookup[ventilation_strategy]
-#        self.__pitch = pitch
-#        self.__orientation = orientation
-#        self.__external_conditions = ext_cond
-#        self.__energy_supply_conn = energy_supply_conn
-#        self.__simulation_time = simulation_time
-
-    def produce_energy(self):
-        """ Produce electrical energy (in kWh) from the PV system
-            according to BS EN 15316-4-3:2017 """
-
-        #solar_irradiance in W/m2
-        solar_irradiance = self.__external_conditions.calculated_total_solar_irradiance(
-            self.__pitch,
-            self.__orientation
-            )
-        #solar_irradiation in kWh/m2
-        solar_irradiation = solar_irradiance * self.__simulation_time.timestep() / units.W_per_kW
-        #reference_solar_irradiance kW/m2
-        ref_solar_irradiance = 1
-
-        #CALCULATION
-        #E.el.pv.out.h = E.sol.pv.h * P.pk * f.perf / I.ref
-        #energy_produced = solar_irradiation * peak_power * system_performance_factor
-        #                    / reference_solar_irradiance
-        #energy produced in kWh
-        energy_produced \
-            = solar_irradiation * self.__peak_power * self.__f_perf / ref_solar_irradiance
-        #add energy produced to the applicable energy supply connection (this will reduce demand)
-        self.__energy_supply_conn.supply_energy(energy_produced)
         
+        #water specific heat in kWh/kg.K
+        self.__Cp = contents.specific_heat_capacity_kWh()
+
+    def collector_reference_area(self):
+        """ Calculate total collector reference area from collector module reference area and
+            number of modules
+            eq 50 of STANDARD """
+
+        #collector reference area in m2
+        self.__area = self.__area_module * self.__modules
+
