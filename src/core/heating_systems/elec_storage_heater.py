3c605617edc4100a97d2fc62b984efc69757a59c
MattCustard <MattCustard@DESKTOP-SVLMSRD.broadband>
2023-01-27 15:11:32 +0000

    Initial creation of the storage heater module.

---
+++ src/core/heating_systems/elec_storage_heater.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+""" TODO Copyright & licensing notices
+
+This module provides object(s) to model the behaviour of electric storage heaters.
+"""
+
+class ElecStorageHeater:
+    """ Class to represent electric storage heaters """
+
+    def __init__(
+            self,
+            rated_power,
+            frac_convective,
+            energy_supply_conn,
+            simulation_time,
+            control,
+            ):
+        """ Construct an ElecStorageHeater object
+
+        Arguments:
+        rated_power        -- in kW
+        frac_convective    -- convective fraction for heating
+        energy_supply_conn -- reference to EnergySupplyConnection object
+        simulation_time    -- reference to SimulationTime object
+        control -- reference to a control object which must implement is_on() and setpnt() funcs
+        """
+        self.__pwr                = rated_power
+        self.__frac_convective    = frac_convective
+        self.__energy_supply_conn = energy_supply_conn
+        self.__simulation_time    = simulation_time
+        self.__control            = control
+
+    def temp_setpnt(self):
+        return self.__control.setpnt()
+
+    def frac_convective(self):
+        return self.__frac_convective
+
+    def demand_energy(self, energy_demand):
+        """ Demand energy (in kWh) from the heater """
+
+        # Account for time control where present. If no control present, assume
+        # system is always active (except for basic thermostatic control, which
+        # is implicit in demand calculation).
+        # TODO Account for manual (or smart) control where heater may be left
+        #      on for longer than it would be under a simple thermostatic
+        #      control?
+        if self.__control is None or self.__control.is_on():
+            # Energy that heater is able to supply is limited by power rating
+            energy_supplied = min(energy_demand, self.__pwr * self.__simulation_time.timestep())
+        else:
+            energy_supplied = 0.0
+
+        self.__energy_supply_conn.demand_energy(energy_supplied)
+        return energy_supplied
