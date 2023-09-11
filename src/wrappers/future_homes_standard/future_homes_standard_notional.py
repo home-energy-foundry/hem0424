e84faf8fce93192355b57235a5b36408af228b7c
Dharm Sivarajah <dharm.sivarajah@bregroup.com>
2023-09-11 19:17:52 +0100

    notional building - updating hot water source
    now includes primary pipework

--- src/wrappers/future_homes_standard/future_homes_standard_notional.py
+++ src/wrappers/future_homes_standard/future_homes_standard_notional.py
@@ -569,4 +569,18 @@ def edit_hot_water_source(project_dict):
                 "utilisation_factor": 0.98
             }
         }
-    
+
+    #Primary pipework
+    if 'primary_pipework' in project_dict['HotWaterSource']['hw cylinder']:
+        primary_pipework_dict = project_dict['HotWaterSource']['hw cylinder']['primary_pipework']
+        TFA = calc_TFA(project_dict)
+        length = primary_pipework_dict['length']
+        length =  min(length, 0.05 * (TFA / 2))
+        primary_pipework_dict['length'] = length
+        primary_pipework_dict['insulation_thermal_conductivity'] = 0.035
+        primary_pipework_dict['surface_reflectivity'] = False
+        
+        insulation_thickness = 0.025
+        if primary_pipework_dict['insulation_thickness_mm'] > 0.025:
+             insulation_thickness = 0.032
+        primary_pipework_dict['insulation_thickness_mm'] = insulation_thickness
