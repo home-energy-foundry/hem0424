e5efd756adc58ab45e76a6da6dff9c346e8223b8
Steven Abnett <steven.abnett@bregroup.com>
2024-03-01 11:22:19 +0000

    Quick fix for direct solar shading calculation
    Calculation was underestimating solar radiation on surfaces with shallow
    pitch away from south

--- src/core/external_conditions.py
+++ src/core/external_conditions.py
@@ -1495,8 +1495,22 @@ class ExternalConditions:
         # first check if the surface is outside the solar beam
         # if so then direct shading is complete and we don't need to
         # calculate shading from objects
+        # TODO The outside solar beam condition is based on a vertical projection
+        #      of the surface and does not account for the condition where a
+        #      surface that is only slightly pitched is exposed to direct solar
+        #      radiation when the sun is high (e.g. a surface pitched slightly
+        #      to the north will be exposed to direct solar radiation when the
+        #      sun is high in the southern sky). As the solar radiation
+        #      calculation already accounts for the situation where the sun is
+        #      actually behind the surface (accounting for the combination of
+        #      orientation and pitch), there is no need to zero it using the
+        #      shading factor. For now, we set the shading factor to 1 and ignore
+        #      shading from objects on the other side of the building (which if
+        #      significantly pitched would have to be relatively tall and/or
+        #      very close to cast a shadow on the surface in question anyway),
+        #      so that results in the unshaded case will be correct.
         if self.outside_solar_beam(tilt, orientation):
-            Fdir = 0
+            Fdir = 1.0
         else:
             Fdir = self.direct_shading_reduction_factor \
                     (base_height, height, width, orientation, window_shading)
